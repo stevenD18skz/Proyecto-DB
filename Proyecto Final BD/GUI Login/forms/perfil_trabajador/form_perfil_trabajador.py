033e14ffa43111634fd9f7da86fba2a84f23f864
MiguelTami <149514850+MiguelTami@users.noreply.github.com>
2023-12-17 23:22:12 -0500

    Update form_perfil_trabajador.py

--- Proyecto Final BD/GUI Login/forms/perfil_trabajador/form_perfil_trabajador.py
+++ Proyecto Final BD/GUI Login/forms/perfil_trabajador/form_perfil_trabajador.py
@@ -7,9 +7,11 @@ import conexionDB
 
 
 class FormPerfilTrabajador(FormPerfilTrabajadorDesigner):
+    # Este es el constructor de la parte visual.
     def __init__(self, nombre_usuario):
         super().__init__(nombre_usuario)
 
     def editar(self, nombre_usuario):
+        # Este método se encarga de editar el perfil del trabajador. Para ello, crea una instancia de la clase FormEditarPerfilTrabajadorDesigner con el nombre de usuario.
         FormEditarPerfilTrabajadorDesigner(nombre_usuario)
 
